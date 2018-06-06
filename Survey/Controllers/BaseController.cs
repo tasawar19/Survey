bbf2ef20639232b09ad83c99b29465cd9fac4199
tasawar19 <tasawar_19@yahoo.com>
2018-06-07 00:07:45 +0500

    Solution setup

---
+++ Survey/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Survey.Models;
+using System.Web.Mvc;
+
+namespace Survey.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected SurveyEntities db;
+        protected int userID;
+        // GET: Base
+        public BaseController()
+        {
+            db = new SurveyEntities();
+            userID = 1;
+        }
+    }
+}
---
+++ Survey/Controllers/SurveysController.cs
@@ -0,0 +1,125 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Survey.Controllers
+{
+    public class SurveysController : BaseController
+    {
+        // GET: Surveys
+        public ActionResult Index()
+        {
+            var surveys = db.Surveys.Where(t => t.UserID == userID);
+            return View(surveys.ToList());
+        }
+
+        // GET: Surveys/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // GET: Surveys/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserEmailID");
+            return View();
+        }
+
+        // POST: Surveys/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Models.Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Surveys.Add(survey);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserEmailID", survey.UserID);
+            return View(survey);
+        }
+
+        // GET: Surveys/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserEmailID", survey.UserID);
+            return View(survey);
+        }
+
+        // POST: Surveys/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Models.Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(survey).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserEmailID", survey.UserID);
+            return View(survey);
+        }
+
+        // GET: Surveys/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        // POST: Surveys/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Models.Survey survey = db.Surveys.Find(id);
+            db.Surveys.Remove(survey);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
